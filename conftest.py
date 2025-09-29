7f505afa7ce8abb9b71195ef0c3a811dba80724f
георги <georgypant@mail.ru>
2025-09-29 16:49:59 +0300

    запушил практику по ооп в новый проект для проверки

---
+++ conftest.py
@@ -0,0 +1,61 @@
+import constant
+import pytest
+import requests
+from faker import Faker
+fake = Faker()
+
+BASE_URL = "https://restful-booker.herokuapp.com"
+HEADERS = {"Content-Type": "application/json", "Accept": "application/json"}
+@pytest.fixture(scope="session")
+def auth_session():
+    session = requests.Session()
+    session.headers.update(HEADERS)
+
+    auth_response = session.post(f"{BASE_URL}/auth", json={"username": "admin", "password": "password123"})
+    assert auth_response.status_code == 200, "Ошибка авторизации, статус код не 200"
+
+    token = auth_response.json().get("token")
+    assert token is not None, "Токен не найден в ответе"
+
+    session.headers.update({"Cookie": f"token={token}"})
+    return session
+
+@pytest.fixture()
+def booking_data():
+    return {
+        "firstname": fake.first_name(),
+        "lastname": fake.last_name(),
+        "totalprice": fake.random_int(min=100, max=10000),
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2024-04-05",
+            "checkout": "2024-04-08"
+        },
+        "additionalneeds": "Breakfast"
+    }
+
+@pytest.fixture()
+def invalid_booking_data():
+    return {
+        "lastname": "орми",
+        "totalprice": 1000,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2024-04-05",
+            "checkout": "2024-04-08"
+        },
+        "additionalneeds": "Breakfast"
+    }
+
+@pytest.fixture()
+def invalid_auth_session():
+    session = requests.Session()
+    session.headers.update(HEADERS)
+
+    auth_response = session.post(f"{BASE_URL}/auth", json={"username": "admin", "password": "password123"})
+    assert auth_response.status_code == 200, "Ошибка авторизации, статус код не 200"
+    token = auth_response.json().get("token")
+    assert token is not None, "Токен не найден в ответе"
+
+    session.headers.update({"Cookie": "token=invalidtoken123"})
+    return session
